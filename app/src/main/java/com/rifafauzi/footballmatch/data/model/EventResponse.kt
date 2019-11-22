f4ad6f26276a0cafe8c657dfea9e80e2ed8b16f0
R Rifa Fauzi Komara <rifafauzi6@gmail.com>
2019-11-22 20:01:41 +0700

    start()

--- app/src/main/java/com/rifafauzi/footballmatch/data/model/EventResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.rifafauzi.footballmatch.data.model
-
-data class EventResponse(
-	val events: List<Events?>? = null
-)
--- app/src/main/java/com/rifafauzi/footballmatch/ui/main/MainActivity.kt
+++ app/src/main/java/com/rifafauzi/footballmatch/ui/main/MainActivity.kt
@@ -10,14 +10,9 @@ import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
-    @Inject
-    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
-
-    override fun supportFragmentInjector(): AndroidInjector<Fragment> = dispatchingAndroidInjector
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
