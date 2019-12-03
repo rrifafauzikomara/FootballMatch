752b29cb8450a2c4934b4db28c0532077ad825f7
R Rifa Fauzi Komara <rifafauzi6@gmail.com>
2019-12-03 14:04:10 +0700

    add dependencies for instrument test

---
+++ app/src/androidTest/java/com/rifafauzi/footballmatch/SearchFragmentTest.kt
@@ -0,0 +1,16 @@
+package com.rifafauzi.footballmatch
+
+import androidx.test.runner.AndroidJUnit4
+import org.junit.runner.RunWith
+
+
+/**
+ * Created by rrifafauzikomara on 2019-12-03.
+ */
+
+@RunWith(AndroidJUnit4::class)
+class SearchFragmentTest {
+
+
+
+}
