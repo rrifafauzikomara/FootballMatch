1361b9cf2e370931de8bb13210e63298daa47a32
R Rifa Fauzi Komara <rifafauzi6@gmail.com>
2019-12-07 11:37:24 +0700

    add show team overview

--- app/src/main/java/com/rifafauzi/footballmatch/ui/teamoverview/TeamOverviewFragment.kt
+++ app/src/main/java/com/rifafauzi/footballmatch/ui/teamoverview/TeamOverviewFragment.kt
@@ -2,13 +2,15 @@ package com.rifafauzi.footballmatch.ui.teamoverview
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import com.rifafauzi.footballmatch.R
 import com.rifafauzi.footballmatch.base.BaseFragment
+import com.rifafauzi.footballmatch.common.Result
 import com.rifafauzi.footballmatch.databinding.FragmentTeamOverviewBinding
+import com.rifafauzi.footballmatch.model.teams.Team
+import com.rifafauzi.footballmatch.utils.ID_TEAM
 
 /**
  * A simple [Fragment] subclass.
@@ -16,7 +18,66 @@ import com.rifafauzi.footballmatch.databinding.FragmentTeamOverviewBinding
 class TeamOverviewFragment : BaseFragment<FragmentTeamOverviewBinding, TeamOverviewViewModel>() {
 
     override fun getLayoutResourceId() = R.layout.fragment_team_overview
-
     override fun getViewModelClass() = TeamOverviewViewModel::class.java
+
+    private var idTeam:String? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        idTeam = arguments?.getString(ID_TEAM)
+
+        vm.getTeamDetail(idTeam)
+        vm.detailTeam.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                when (it) {
+                    is Result.Loading -> {
+                        hideTeam()
+                        showLoading()
+                    }
+                    is Result.HasData -> {
+                        showTeam()
+                        hideLoading()
+                        displayData(it.data)
+                    }
+                    is Result.NoData -> {
+                        hideTeam()
+                        hideLoading()
+                        longSnackBar(resources.getString(R.string.empty_data))
+                    }
+                    is Result.Error -> {
+                        hideTeam()
+                        hideLoading()
+                        longSnackBar(resources.getString(R.string.unknown_error))
+                    }
+                    is Result.NoInternetConnection -> {
+                        hideTeam()
+                        hideLoading()
+                        longSnackBar(resources.getString(R.string.no_connection))
+                    }
+                }
+            }
+        })
+    }
+
+    private fun displayData(data: List<Team>) {
+        binding.data = data[0]
+    }
+
+    private fun showLoading() {
+        binding.showLoading = true
+    }
+
+    private fun hideLoading() {
+        binding.showLoading = false
+    }
+
+    private fun showTeam() {
+        binding.showData = true
+    }
+
+    private fun hideTeam() {
+        binding.showData = false
+    }
 
 }
--- app/src/main/java/com/rifafauzi/footballmatch/ui/teamoverview/TeamOverviewViewModel.kt
+++ app/src/main/java/com/rifafauzi/footballmatch/ui/teamoverview/TeamOverviewViewModel.kt
@@ -1,11 +1,85 @@
 package com.rifafauzi.footballmatch.ui.teamoverview
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.rifafauzi.footballmatch.R
+import com.rifafauzi.footballmatch.base.BaseResponse
 import com.rifafauzi.footballmatch.base.BaseViewModel
+import com.rifafauzi.footballmatch.common.Result
+import com.rifafauzi.footballmatch.model.teams.Team
+import com.rifafauzi.footballmatch.model.teams.TeamResponse
 import com.rifafauzi.footballmatch.repository.teams.TeamsRepository
+import com.rifafauzi.footballmatch.utils.plusAssign
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 /**
  * Created by rrifafauzikomara on 2019-12-06.
  */
  
-class TeamOverviewViewModel @Inject constructor(private val repository: TeamsRepository) : BaseViewModel()
+class TeamOverviewViewModel @Inject constructor(private val repository: TeamsRepository) : BaseViewModel() {
+
+    private val _detailTeam = MutableLiveData<Result<List<Team>>>()
+    val detailTeam: LiveData<Result<List<Team>>>
+        get() = _detailTeam
+
+    fun getTeamDetail(idTeam: String?) {
+        mCompositeDisposable += repository.getTeam(idTeam)
+            .map { transformData(it) }
+            .doOnSubscribe { setResultDetailTeam(Result.Loading()) }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeWith(object : BaseResponse<List<Team>>() {
+                override fun onSuccess(response: List<Team>) {
+                    if (response.isEmpty()) {
+                        setResultDetailTeam(Result.NoData())
+                        return
+                    }
+                    setResultDetailTeam(Result.HasData(response))
+                }
+
+                override fun onNoInternetConnection() {
+                    setResultDetailTeam(Result.NoInternetConnection())
+                }
+
+                override fun onTimeout() {
+                    setResultDetailTeam(Result.Error(R.string.timeout))
+                }
+
+                override fun onUnknownError(message: String) {
+                    setResultDetailTeam(Result.Error(R.string.unknown_error))
+                }
+
+            })
+    }
+
+    private fun transformData(data: TeamResponse) : List<Team> {
+        val teams = mutableListOf<Team>()
+        for (i in data.teams) {
+            teams.add(
+                Team(
+                    i.idTeam,
+                    i.strTeamBadge,
+                    i.strTeam,
+                    i.strDescriptionEN,
+                    i.strLeague,
+                    i.strStadium,
+                    i.strStadiumLocation,
+                    i.strStadiumDescription,
+                    i.intStadiumCapacity,
+                    i.strCountry,
+                    i.strStadiumThumb,
+                    i.strTeamJersey,
+                    i.strTeamBanner
+                )
+            )
+        }
+        return teams.toList()
+    }
+
+    private fun setResultDetailTeam(result: Result<List<Team>>) {
+        _detailTeam.postValue(result)
+    }
+
+}
